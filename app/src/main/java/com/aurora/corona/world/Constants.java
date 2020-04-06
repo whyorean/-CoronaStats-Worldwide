8682d573f2d96c1b22e14cb7e5352054e76eb885
Rahul Kumar Patel <whyorean@gmail.com>
2020-04-06 23:34:50 +0530

    Remove un-neccessary constants

--- app/src/main/java/com/aurora/corona/world/Constants.java
+++ app/src/main/java/com/aurora/corona/world/Constants.java
@@ -22,17 +22,7 @@ public abstract class Constants {
 
     public static final String TAG = "Corona Stats";
     public static final String SHARED_PREFERENCES_KEY = "com.aurora.corona.world";
-
-    public static final String PREFERENCE_OVERALL_DATA = "PREFERENCE_OVERALL_DATA";
-    public static final String PREFERENCE_CASE_TIME_SERIES = "PREFERENCE_CASE_TIME_SERIES";
-    public static final String PREFERENCE_KEY_VALUES = "PREFERENCE_KEY_VALUES";
     public static final String PREFERENCE_STATE_WISE = "PREFERENCE_STATE_WISE";
-    public static final String PREFERENCE_TESTED = "PREFERENCE_TESTED";
-
-    public static final String PREFERENCE_ACTIVE_CASES = "PREFERENCE_ACTIVE_CASES";
-    public static final String PREFERENCE_CLOSED_CASES = "PREFERENCE_CLOSED_CASES";
-    public static final String PREFERENCE_TABLE = "PREFERENCE_TABLE";
-    public static final String PREFERENCE_TABLE_WORLD = "PREFERENCE_TABLE_WORLD";
 
     public static final String PREFERENCE_NINJA_GLOBAL = "PREFERENCE_NINJA_GLOBAL";
     public static final String PREFERENCE_NINJA_GLOBAL_AVAILABLE = "PREFERENCE_NINJA_GLOBAL_AVAILABLE";
@@ -51,9 +41,6 @@ public abstract class Constants {
     public static final String PREFERENCE_COVID19_COUNTRIES = "PREFERENCE_COVID19_COUNTRIES";
     public static final String PREFERENCE_COVID19_COUNTRIES_AVAILABLE = "PREFERENCE_COVID19_COUNTRIES_AVAILABLE";
     public static final String PREFERENCE_COVID19_COUNTRY_SELECTED = "PREFERENCE_COVID19_COUNTRY_SELECTED";
-
-    public static final String PREFERENCE_DATA_AVAILABLE = "PREFERENCE_DATA_AVAILABLE";
-    public static final String PREFERENCE_LAST_UPDATED = "PREFERENCE_LAST_UPDATED";
 
     public static final String INT_EXTRA = "INT_EXTRA";
     public static final String FLOAT_EXTRA = "FLOAT_EXTRA";
